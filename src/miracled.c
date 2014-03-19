87b804c4817214eb849cf02410dd3385b6d1b9b8
David Herrmann <dh.herrmann@gmail.com>
2014-03-19 13:31:01 +0100

    Remove old wifi code in miracled
    The wifi code is now moved to miracle-wifid, no need to keep it anymore.
    
    Signed-off-by: David Herrmann <dh.herrmann@gmail.com>

--- src/miracled.c
+++ src/miracled.c
@@ -3,30 +3,22 @@
  *
  * Copyright (c) 2013-2014 David Herrmann <dh.herrmann@gmail.com>
  *
- * Permission is hereby granted, free of charge, to any person obtaining
- * a copy of this software and associated documentation files
- * (the "Software"), to deal in the Software without restriction, including
- * without limitation the rights to use, copy, modify, merge, publish,
- * distribute, sublicense, and/or sell copies of the Software, and to
- * permit persons to whom the Software is furnished to do so, subject to
- * the following conditions:
+ * MiracleCast is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
  *
- * The above copyright notice and this permission notice shall be included
- * in all copies or substantial portions of the Software.
+ * MiracleCast is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
- * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
- * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
- * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
- * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
- * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with MiracleCast; If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <errno.h>
 #include <getopt.h>
-#include <libudev.h>
-#include <libwfd.h>
 #include <signal.h>
 #include <stdbool.h>
 #include <stdio.h>
@@ -39,373 +31,26 @@
 #include <time.h>
 #include <unistd.h>
 #include "miracled.h"
-#include "shl_htable.h"
 #include "shl_macro.h"
 #include "shl_log.h"
 #include "shl_util.h"
 
-bool arg_manage_wifi = false;
-const char *arg_wpa_rundir = "/run/wpa_supplicant";
-const char *arg_wpa_bindir = "/usr/bin";
-
-/*
- * Peer Handling
- */
-
-struct peer *manager_find_peer(struct manager *m, const char *name)
-{
-	char **elem;
-	bool res;
-
-	res = shl_htable_lookup_str(&m->peers, name, NULL, &elem);
-	if (!res)
-		return NULL;
-
-	return peer_from_htable(elem);
-}
-
-/*
- * Link Handling
- */
-
-struct link *manager_find_link(struct manager *m, const char *name)
-{
-	char **elem;
-	bool res;
-
-	res = shl_htable_lookup_str(&m->links, name, NULL, &elem);
-	if (!res)
-		return NULL;
-
-	return link_from_htable(elem);
-}
-
-/*
- * Manager Handling
- */
-
-static void manager_add_link_from_udev(struct manager *m,
-				       struct udev_device *d)
-{
-	struct link *l;
-
-	if (!udev_device_has_tag(d, "miracle"))
-		return;
-
-	log_debug("link %s tagged via udev",
-		  udev_device_get_sysname(d));
-
-	link_new(m,
-		 LINK_WIFI,
-		 udev_device_get_sysname(d),
-		 &l);
-}
-
-static void manager_remove_link_from_udev(struct manager *m,
-					  struct udev_device *d)
-{
-	_shl_free_ char *name = NULL;
-	int r;
-	struct link *l;
-
-	r = link_make_name(LINK_WIFI, udev_device_get_sysname(d), &name);
-	if (r < 0)
-		return log_vERR(r);
-
-	l = manager_find_link(m, name);
-	if (!l)
-		return;
-
-	log_debug("link %s removed via udev", name);
-
-	link_free(l);
-}
-
-static int manager_udev_fn(sd_event_source *source,
-			   int fd,
-			   uint32_t mask,
-			   void *data)
-{
-	struct manager *m = data;
-	struct udev_device *d = NULL;
-	const char *action;
-
-	d = udev_monitor_receive_device(m->udev_mon);
-	if (!d)
-		goto out;
-
-	action = udev_device_get_action(d);
-	if (!action)
-		goto out;
-
-	if (!strcmp(action, "add") && arg_manage_wifi)
-		manager_add_link_from_udev(m, d);
-	else if (!strcmp(action, "remove"))
-		manager_remove_link_from_udev(m, d);
-
-out:
-	udev_device_unref(d);
-	return 0;
-}
-
-static int manager_signal_fn(sd_event_source *source,
-			     const struct signalfd_siginfo *ssi,
-			     void *data)
-{
-	struct manager *m = data;
-
-	if (ssi->ssi_signo == SIGCHLD) {
-		log_debug("caught SIGCHLD for %d", (int)ssi->ssi_pid);
-		return 0;
-	}
-
-	log_notice("caught signal %d, exiting..", (int)ssi->ssi_signo);
-	sd_event_exit(m->event, 0);
-
-	return 0;
-}
-
 static void manager_free(struct manager *m)
 {
-	unsigned int i;
-	struct link *l;
-
 	if (!m)
 		return;
 
-	while ((l = MANAGER_FIRST_LINK(m)))
-		link_free(l);
-
-	shl_htable_clear_str(&m->links, NULL, NULL);
-	shl_htable_clear_str(&m->peers, NULL, NULL);
-
-	manager_dbus_disconnect(m);
-
-	sd_event_source_unref(m->udev_mon_source);
-	udev_monitor_unref(m->udev_mon);
-	udev_unref(m->udev);
-
-	for (i = 0; m->sigs[i]; ++i)
-		sd_event_source_unref(m->sigs[i]);
-	sd_bus_unref(m->bus);
-	sd_event_unref(m->event);
-	free(m->friendly_name);
 	free(m);
 }
 
 static int manager_new(struct manager **out)
 {
-	char buf[64] = { };
-	struct manager *m;
-	static const int sigs[] = {
-		SIGINT, SIGTERM, SIGQUIT, SIGHUP, SIGPIPE, SIGCHLD, 0
-	};
-	unsigned int i;
-	sigset_t mask;
-	int r;
-
-	m = calloc(1, sizeof(*m));
-	if (!m)
-		return log_ENOMEM();
-
-	shl_htable_init_str(&m->links);
-	shl_htable_init_str(&m->peers);
-
-	snprintf(buf, sizeof(buf) - 1, "unknown-%u", rand());
-	m->friendly_name = strdup(buf);
-	if (!m->friendly_name) {
-		r = log_ENOMEM();
-		goto error;
-	}
-
-	r = sd_event_default(&m->event);
-	if (r < 0) {
-		log_vERR(r);
-		goto error;
-	}
-
-	r = sd_event_set_watchdog(m->event, true);
-	if (r < 0) {
-		log_vERR(r);
-		goto error;
-	}
-
-	r = sd_bus_default_system(&m->bus);
-	if (r < 0) {
-		log_error("cannot connect to system bus: %d", r);
-		goto error;
-	}
-
-	r = sd_bus_attach_event(m->bus, m->event, 0);
-	if (r < 0) {
-		log_vERR(r);
-		goto error;
-	}
-
-	for (i = 0; sigs[i]; ++i) {
-		sigemptyset(&mask);
-		sigaddset(&mask, sigs[i]);
-		sigprocmask(SIG_BLOCK, &mask, NULL);
-
-		r = sd_event_add_signal(m->event,
-					&m->sigs[i],
-					sigs[i],
-					manager_signal_fn,
-					m);
-		if (r < 0) {
-			log_vERR(r);
-			goto error;
-		}
-	}
-
-	m->udev = udev_new();
-	if (!m->udev) {
-		r = log_ENOMEM();
-		goto error;
-	}
-
-	m->udev_mon = udev_monitor_new_from_netlink(m->udev, "udev");
-	if (!m->udev_mon) {
-		r = log_ENOMEM();
-		goto error;
-	}
-
-	r = udev_monitor_filter_add_match_subsystem_devtype(m->udev_mon,
-							    "net",
-							    "wlan");
-	if (r < 0) {
-		log_vERR(r);
-		goto error;
-	}
-
-	r = udev_monitor_enable_receiving(m->udev_mon);
-	if (r < 0) {
-		log_vERR(r);
-		goto error;
-	}
-
-	r = sd_event_add_io(m->event,
-			    &m->udev_mon_source,
-			    udev_monitor_get_fd(m->udev_mon),
-			    EPOLLHUP | EPOLLERR | EPOLLIN,
-			    manager_udev_fn,
-			    m);
-	if (r < 0) {
-		log_vERR(r);
-		goto error;
-	}
-
-	r = manager_dbus_connect(m);
-	if (r < 0)
-		goto error;
-
-	*out = m;
-	return 0;
-
-error:
-	manager_free(m);
-	return r;
-}
-
-static void manager_read_name(struct manager *m)
-{
-	_cleanup_sd_bus_error_ sd_bus_error err = SD_BUS_ERROR_NULL;
-	_cleanup_sd_bus_message_ sd_bus_message *rep = NULL;
-	const char *name;
-	char *str;
-	int r;
-
-	r = sd_bus_call_method(m->bus,
-			       "org.freedesktop.hostname1",
-			       "/org/freedesktop/hostname1",
-			       "org.freedesktop.DBus.Properties",
-			       "Get",
-			       &err,
-			       &rep,
-			       "ss", "org.freedesktop.hostname1", "Hostname");
-	if (r < 0) {
-		return;
-	}
-
-	r = sd_bus_message_enter_container(rep, 'v', "s");
-	if (r < 0)
-		goto error;
-
-	r = sd_bus_message_read(rep, "s", &name);
-	if (r < 0)
-		goto error;
-
-	if (!name || !*name) {
-		log_warning("no hostname set on systemd.hostname1, using: %s",
-			    m->friendly_name);
-		return;
-	}
-
-	str = strdup(name);
-	if (!str)
-		return log_vENOMEM();
-
-	free(m->friendly_name);
-	m->friendly_name = str;
-	log_debug("friendly-name from local hostname: %s", str);
-
-	return;
-
-error:
-	log_warning("cannot read hostname from systemd.hostname1: %s",
-		    bus_error_message(&err, r));
-}
-
-static void manager_read_links(struct manager *m)
-{
-	struct udev_enumerate *e = NULL;
-	struct udev_list_entry *l;
-	struct udev_device *d;
-	int r;
-
-	e = udev_enumerate_new(m->udev);
-	if (!e)
-		goto error;
-
-	r = udev_enumerate_add_match_subsystem(e, "net");
-	if (r < 0)
-		goto error;
-
-	r = udev_enumerate_add_match_property(e, "DEVTYPE", "wlan");
-	if (r < 0)
-		goto error;
-
-	r = udev_enumerate_add_match_is_initialized(e);
-	if (r < 0)
-		goto error;
-
-	r = udev_enumerate_scan_devices(e);
-	if (r < 0)
-		goto error;
-
-	udev_list_entry_foreach(l, udev_enumerate_get_list_entry(e)) {
-		d = udev_device_new_from_syspath(m->udev,
-						 udev_list_entry_get_name(l));
-		if (!d)
-			goto error;
-
-		manager_add_link_from_udev(m, d);
-		udev_device_unref(d);
-	}
-
-	goto out;
-
-error:
-	log_warning("cannot enumerate links via udev");
-out:
-	udev_enumerate_unref(e);
+	return -EINVAL;
 }
 
 static int manager_run(struct manager *m)
 {
-	manager_read_name(m);
-	manager_read_links(m);
-	return sd_event_loop(m->event);
+	return 0;
 }
 
 static int help(void)
@@ -415,16 +60,11 @@ static int help(void)
 	 *      01234567890123456789012345678901234567890123456789012345678901234567890123456789
 	 */
 	printf("%s [OPTIONS...] ...\n\n"
-	       "Wifi-Display Daemon.\n\n"
+	       "Remote-display Management-daemon.\n\n"
 	       "  -h --help             Show this help\n"
 	       "     --version          Show package version\n"
 	       "     --log-level <lvl>  Maximum level for log messages\n"
 	       "     --log-time         Prefix log-messages with timestamp\n"
-	       "\n"
-	       "     --manage-wifi      Enable internal wifi-management via wpa_supplicant if\n"
-	       "                        they're not managed externally (like NetworkManager)\n"
-	       "     --wpa-rundir <dir> wpa_supplicant runtime dir [/run/wpa_supplicant]\n"
-	       "     --wpa-bindir <dir> wpa_supplicant binary dir [/usr/bin]\n"
 	       , program_invocation_short_name);
 	/*
 	 * 80-char barrier:
@@ -440,20 +80,12 @@ static int parse_argv(int argc, char *argv[])
 		ARG_VERSION = 0x100,
 		ARG_LOG_LEVEL,
 		ARG_LOG_TIME,
-
-		ARG_MANAGE_WIFI,
-		ARG_WPA_RUNDIR,
-		ARG_WPA_BINDIR,
 	};
 	static const struct option options[] = {
 		{ "help",	no_argument,		NULL,	'h' },
 		{ "version",	no_argument,		NULL,	ARG_VERSION },
 		{ "log-level",	required_argument,	NULL,	ARG_LOG_LEVEL },
 		{ "log-time",	no_argument,		NULL,	ARG_LOG_TIME },
-
-		{ "manage-wifi",	no_argument,		NULL,	ARG_MANAGE_WIFI },
-		{ "wpa-rundir",		required_argument,	NULL,	ARG_WPA_RUNDIR },
-		{ "wpa-bindir",		required_argument,	NULL,	ARG_WPA_BINDIR },
 		{}
 	};
 	int c;
@@ -470,16 +102,6 @@ static int parse_argv(int argc, char *argv[])
 			break;
 		case ARG_LOG_TIME:
 			log_init_time();
-			break;
-
-		case ARG_MANAGE_WIFI:
-			arg_manage_wifi = true;
-			break;
-		case ARG_WPA_RUNDIR:
-			arg_wpa_rundir = optarg;
-			break;
-		case ARG_WPA_BINDIR:
-			arg_wpa_bindir = optarg;
 			break;
 		case '?':
 			return -EINVAL;
--- src/miracled.h
+++ src/miracled.h
@@ -3,178 +3,33 @@
  *
  * Copyright (c) 2013-2014 David Herrmann <dh.herrmann@gmail.com>
  *
- * Permission is hereby granted, free of charge, to any person obtaining
- * a copy of this software and associated documentation files
- * (the "Software"), to deal in the Software without restriction, including
- * without limitation the rights to use, copy, modify, merge, publish,
- * distribute, sublicense, and/or sell copies of the Software, and to
- * permit persons to whom the Software is furnished to do so, subject to
- * the following conditions:
+ * MiracleCast is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
  *
- * The above copyright notice and this permission notice shall be included
- * in all copies or substantial portions of the Software.
+ * MiracleCast is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
- * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
- * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
- * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
- * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
- * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with MiracleCast; If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <errno.h>
-#include <libudev.h>
 #include <stdbool.h>
 #include <stdlib.h>
 #include <systemd/sd-bus.h>
 #include <systemd/sd-event.h>
-#include "miracle.h"
-#include "shl_dlist.h"
-#include "shl_htable.h"
 
 #ifndef MIRACLED_H
 #define MIRACLED_H
-
-struct manager;
-struct link;
-struct peer;
-struct wifi;
-struct wifi_dev;
-struct wifi_event;
-
-/* peer */
-
-struct peer {
-	struct shl_dlist list;
-	struct link *l;
-	unsigned int id;
-	char *name;
-
-	struct wifi_dev *d;
-};
-
-#define peer_from_htable(_p) \
-	shl_htable_offsetof((_p), struct peer, name)
-#define peer_from_list(_p) \
-	shl_dlist_entry((_p), struct peer, list)
-
-int peer_make_name(unsigned int id, char **out);
-
-int peer_new_wifi(struct link *l, struct wifi_dev *d, struct peer **out);
-void peer_free(struct peer *p);
-
-const char *peer_get_friendly_name(struct peer *p);
-bool peer_is_connected(struct peer *p);
-const char *peer_get_interface(struct peer *p);
-const char *peer_get_local_address(struct peer *p);
-const char *peer_get_remote_address(struct peer *p);
-
-void peer_process_wifi(struct peer *p, struct wifi_event *ev);
-
-int peer_allow(struct peer *p, const char *pin);
-void peer_reject(struct peer *p);
-int peer_connect(struct peer *p, const char *prov, const char *pin);
-void peer_disconnect(struct peer *p);
-
-/* link */
-
-enum link_type {
-	LINK_VIRTUAL,
-	LINK_WIFI,
-	LINK_CNT,
-};
-
-struct link {
-	struct manager *m;
-	unsigned int type;
-	char *interface;
-	char *name;
-	char *friendly_name;
-
-	struct shl_dlist peers;
-
-	struct wifi *w;
-	sd_event_source *wpa_child_source;
-	sd_event_source *wpa_startup_source;
-	unsigned int wpa_startup_attempts;
-
-	bool running : 1;
-};
-
-#define link_from_htable(_l) \
-	shl_htable_offsetof((_l), struct link, name)
-#define LINK_FIRST_PEER(_l) (shl_dlist_empty(&(_l)->peers) ? \
-	NULL : peer_from_list((_l)->peers.next))
-
-const char *link_type_to_str(unsigned int type);
-unsigned int link_type_from_str(const char *str);
-int link_make_name(unsigned int type, const char *interface, char **out);
-
-int link_new(struct manager *m,
-	     unsigned int type,
-	     const char *interface,
-	     struct link **out);
-void link_free(struct link *l);
-
-int link_set_friendly_name(struct link *l, const char *name);
-
-int link_start_scan(struct link *l);
-void link_stop_scan(struct link *l);
 
 /* manager */
 
 struct manager {
 	sd_event *event;
-	sd_bus *bus;
-	sd_event_source *sigs[_NSIG];
-	struct udev *udev;
-	struct udev_monitor *udev_mon;
-	sd_event_source *udev_mon_source;
-
-	unsigned int peer_ids;
-
-	size_t link_cnt;
-	size_t peer_cnt;
-	struct shl_htable links;
-	struct shl_htable peers;
-
-	char *friendly_name;
 };
-
-#define MANAGER_FIRST_LINK(_m) \
-	SHL_HTABLE_FIRST_MACRO(&(_m)->links, link_from_htable)
-#define MANAGER_FOREACH_LINK(_i, _m) \
-	SHL_HTABLE_FOREACH_MACRO(_i, &(_m)->links, link_from_htable)
-#define MANAGER_FOREACH_PEER(_i, _m) \
-	SHL_HTABLE_FOREACH_MACRO(_i, &(_m)->peers, peer_from_htable)
-
-int manager_dbus_connect(struct manager *m);
-void manager_dbus_disconnect(struct manager *m);
-
-struct link *manager_find_link(struct manager *m, const char *name);
-struct peer *manager_find_peer(struct manager *m, const char *name);
-
-/* dbus */
-
-void peer_dbus_provision_request(struct peer *p,
-				 const char *type,
-				 const char *pin);
-_shl_sentinel_
-void peer_dbus_properties_changed(struct peer *p, const char *prop, ...);
-void peer_dbus_added(struct peer *p);
-void peer_dbus_removed(struct peer *p);
-
-_shl_sentinel_
-void link_dbus_properties_changed(struct link *l, const char *prop, ...);
-void link_dbus_scan_stopped(struct link *l);
-void link_dbus_added(struct link *l);
-void link_dbus_removed(struct link *l);
-
-/* cli arguments */
-
-extern bool arg_manage_wifi;
-extern const char *arg_wpa_rundir;
-extern const char *arg_wpa_bindir;
 
 #endif /* MIRACLED_H */
