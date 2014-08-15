066eca8638f1bf498c50f4d8f491b027e35364f0
Andrey Gusakov <adnrey.gusakov@cogentembedded.com>
2014-08-15 08:56:13 +0400

    RTSP: close message header and body on fail
    Signed-off-by: Andrey Gusakov <andrey.gusakov@cogentembedded.com>

--- src/shared/rtsp.c
+++ src/shared/rtsp.c
@@ -1588,8 +1588,13 @@ int rtsp_message_readv(struct rtsp_message *m,
 
 	for ( ; *types; ++types) {
 		r = rtsp_message_readv_basic(m, *types, args);
-		if (r < 0)
+		if (r < 0) {
+			if (m->iter_body)
+				rtsp_message_exit_body(m);
+			if (m->iter_header)
+				rtsp_message_exit_header(m);
 			return r;
+		}
 	}
 
 	return 0;
