279cc7d73ae4916b4faaf7ba394e1e613a3be365
Alberto Fanjul <alberto.fanjul@gmail.com>
2014-09-03 01:38:44 +0200

    tester for gstreamer viewer
    miraclecast depends on gstreamer an a bunch of its plugins to show
    screen mirrored. This script provides a test to confirm everything
    needed is installed, and suggest another way to show streamed
    source if its needed plugins are not available.

---
+++ res/test_viewer.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+#
+# test gstreamer plugins required for miraclecast
+#
+
+plugins=(udpsrc rtpjitterbuffer rtpmp2tdepay tsdemux h264parse avdec_h264 autovideosink)
+
+echo testing plugins required
+echo
+
+FAIL=
+
+for plugin in ${plugins[@]}
+do
+   gst-inspect-1.0 $plugin &> /dev/null
+   if [ $? != 0 ]
+   then
+      FAIL=1
+      echo -e \\tgst plugin \"$plugin\" not available
+   fi
+done
+
+echo 
+
+if [ -v FAIL ]
+then
+   cat <<EOF 
+Some plugins required for visualization are missed
+
+Try installing packages "gst-plugins-bad, gst-plugins-base, gst-plugins-base-libs, gst-plugins-good, gst-plugins-ugly, gst-libav, gstreamer".
+
+If that fails too, try:
+
+$ vlc rtp://@1991
+
+EOF
+fi
+
+
+
