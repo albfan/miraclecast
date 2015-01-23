740a9de08071d83a5f96f503ebc881f7f5f32952
albfan <albertofanjul@gmail.com>
2015-01-23 01:28:15 +0100

    fix view tester

--- res/test_viewer.sh
+++ res/test_viewer.sh
@@ -1,29 +1,32 @@
 #!/bin/bash
-
 #
 # test gstreamer plugins required for miraclecast
 #
 
 plugins=(udpsrc rtpjitterbuffer rtpmp2tdepay tsdemux h264parse avdec_h264 autovideosink)
 
-echo testing plugins required
+echo testing plugins required:
 echo
 
 FAIL=
 
 for plugin in ${plugins[@]}
 do
+   echo -n test $plugin...
    gst-inspect-1.0 $plugin &> /dev/null
    if [ $? != 0 ]
    then
       FAIL=1
+      echo
       echo -e \\tgst plugin \"$plugin\" not available
+   else
+      echo -e " (passed)"
    fi
 done
 
 echo 
 
-if [ -v FAIL ]
+if [ -n "$FAIL" ]
 then
    cat <<EOF 
 Some plugins required for visualization are missed
@@ -35,7 +38,9 @@ If that fails too, try:
 $ vlc rtp://@1991
 
 EOF
+else
+   echo everything installed
 fi
-
+echo
 
 
