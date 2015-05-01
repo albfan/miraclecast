0f2e51af91db2418278029f031eafe93b247825a
albfan <albertofanjul@gmail.com>
2015-05-01 22:12:17 +0200

    verbose killing previous wpa_supplicant connection

--- res/kill-wpa.sh
+++ res/kill-wpa.sh
@@ -1,2 +1,15 @@
-echo killing actual wpa_supplicant connection
-sudo kill -9 $(ps -ef | grep wpa_supplican[t] | awk '{print $2}')
+#!/bin/bash
+
+. miracle-utils.sh
+
+kill_ubuntu_network_manager
+
+WPA_PID=$(find_wpa_supplicant_pid)
+if [ -n "$WPA_PID" ]
+then
+   echo killing existing wpa_supplicant connection
+   sudo kill -9 $WPA_PID
+else
+   echo cannot find wpa_supplicant connection to kill
+   exit 1
+fi
