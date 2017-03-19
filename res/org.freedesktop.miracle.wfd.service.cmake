f0920c4c799a72c33dd8fcd118d5a94dba460bda
Derek Dai <daiderek@gmail.com>
2017-03-19 13:39:22 +0800

    rename miracle-wfdctl to miracle-dispd

--- res/org.freedesktop.miracle.wfd.service.cmake
+++ res/org.freedesktop.miracle.wfd.service.cmake
@@ -1,5 +1,5 @@
 [D-BUS Service]
 Name=org.freedesktop.miracle.wfd
-Exec=/bin/sh -c 'PATH=/sbin:/usr/bin LOG_LEVEL=trace @CMAKE_INSTALL_PREFIX@/bin/miracle-wfdctl'
+Exec=/bin/sh -c 'LOG_LEVEL=trace @CMAKE_INSTALL_PREFIX@/bin/miracle-dispd'
 User=root
 SystemdService=dbus-org.freedesktop.miracle.wfd.service
