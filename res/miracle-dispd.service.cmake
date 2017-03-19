b21aa0d0397aadfd08163373ed2ed37bcc8390de
Derek Dai <daiderek@gmail.com>
2017-03-19 13:39:22 +0800

    rename miracle-wfdctl to miracle-dispd

--- res/miracle-dispd.service.cmake
+++ res/miracle-dispd.service.cmake
@@ -1,14 +1,13 @@
 [Unit]
-Description=Miraclecast WiFi Display Controller
+Description=Miraclecast WiFi Display Service
 After=dbus.service
 Requires=miracle-wifid.service
 After=miracle-wifid.service
 
 [Service]
 BusName=org.freedesktop.miracle.wfd
-Environment=PATH=/sbin:/usr/bin
 Environment=LOG_LEVEL=trace
-ExecStart=@CMAKE_INSTALL_PREFIX@/bin/miracle-wfdctl
+ExecStart=@CMAKE_INSTALL_PREFIX@/bin/miracle-dispd
 
 [Install]
 WantedBy=multi-user.target
