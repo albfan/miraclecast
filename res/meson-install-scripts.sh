1e9180a44adb0431ef31a541a30fef17e9a1cf32
Derek Dai <daiderek@gmail.com>
2017-04-03 22:08:51 +0800

    remove install script and install scripts with install_data()

--- res/meson-install-scripts.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-RESDIR="${MESON_SOURCE_ROOT}/res"
-BINDIR="${MESON_INSTALL_DESTDIR_PREFIX}/$1"
-
-install -dv "$BINDIR"
-install -v "$RESDIR"/miracle-gst "$BINDIR"
-install -v "$RESDIR"/gstplayer "$BINDIR"
-install -v "$RESDIR"/uibc-viewer "$BINDIR"
