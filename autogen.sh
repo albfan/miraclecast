84d602793db4c070a8be3c43441fc58309d2e356
albfan <albertofanjul@gmail.com>
2017-04-15 17:30:47 +0200

    Install dbus policy
    closes #107

--- autogen.sh
+++ autogen.sh
@@ -1,2 +1,62 @@
 #!/bin/sh
-autoreconf -f --install
+
+set -e
+
+oldpwd=$(pwd)
+topdir=$(dirname $0)
+cd $topdir
+
+#intltoolize --force --automake
+autoreconf --force --install --symlink
+
+libdir() {
+  echo $(cd "$1/$(gcc -print-multi-os-directory)"; pwd)
+}
+
+args="\
+--sysconfdir=/etc \
+--localstatedir=/var \
+--libdir=$(libdir /usr/lib) \
+"
+
+if [ -f "$topdir/.config.args" ]; then
+  args="$args $(cat $topdir/.config.args)"
+fi
+
+cd $oldpwd
+
+if [ "x$1" = "xc" ]; then
+  $topdir/configure CFLAGS='-g -O0 -ftrapv' $args
+  make clean
+elif [ "x$1" = "xg" ]; then
+  $topdir/configure CFLAGS='-g -Og -ftrapv' $args
+  make clean
+elif [ "x$1" = "xa" ]; then
+  $topdir/configure CFLAGS='-g -O0 -Wsuggest-attribute=pure -Wsuggest-attribute=const -ftrapv' $args
+  make clean
+elif [ "x$1" = "xl" ]; then
+  $topdir/configure CC=clang CFLAGS='-g -O0 -ftrapv' $args
+  make clean
+elif [ "x$1" = "xs" ]; then
+  scan-build $topdir/configure CFLAGS='-std=gnu99 -g -O0 -ftrapv' $args
+  scan-build make
+else
+  cat <<EOF
+
+----------------------------------------------------------------
+Initialized build system. For a common configuration please run:
+----------------------------------------------------------------
+
+$topdir/configure CFLAGS='-g -O0 -ftrapv' $args
+
+or run $0 with param:
+
+- c: compilation
+- g: debugging
+- a: pure/const warning
+- l: clang build
+- s: scan-build reporting
+
+EOF
+
+fi
