0d907cf8663322ab67a7a8166ab11c8874647147
Derek Dai <daiderek@gmail.com>
2017-05-10 13:20:51 +0800

    miracle-wifid: reduce caps from .service file
    Change-Id: I8434fe9dfcf141156635d7a6f8a9c2aaa4469263

--- res/miracle-wifid.service.cmake
+++ res/miracle-wifid.service.cmake
@@ -6,7 +6,13 @@ Requires=network.target
 [Service]
 BusName=org.freedesktop.miracle.wifi
 Environment=PATH=/sbin:/usr/bin
-ExecStart=@CMAKE_INSTALL_PREFIX@/bin/miracle-wifid --use-dev --log-level trace --lazy-managed
+ExecStart=@CMAKE_INSTALL_PREFIX@/bin/miracle-wifid \
+	--use-dev \
+	--log-level trace \
+	--lazy-managed
+CapabilityBoundingSet=CAP_NET_ADMIN \
+	CAP_NET_BIND_SERVICE \
+	CAP_NET_RAW
 
 [Install]
 WantedBy=multi-user.target
