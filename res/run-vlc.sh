bdc49b5b4c7f40819fdea920cd22407d31de00ab
Avinash Pandey <avinash@eximhub.co>
2023-09-21 21:51:31 +0530

    added installation script; modified service files

--- res/run-vlc.sh
+++ res/run-vlc.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
+
 export DISPLAY=:0 && su -c "cvlc --no-osd --no-video-title-show --network-caching=200 rtp://@:7236" letsving
 #export DISPLAY=:0 && su -c "cvlc --no-osd --no-video-title-show --network-caching=200 --no-ts-trust-pcr --h264-fps=23 rtp://@:7236" letsving
