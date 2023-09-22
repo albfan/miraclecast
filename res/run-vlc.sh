929ed8758efe31acfdac22163cc2ce0b5ab4b3f9
Avinash Pandey <avinash@eximhub.co>
2023-09-22 16:09:36 +0530

    fixed audio bug

--- res/run-vlc.sh
+++ res/run-vlc.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-export DISPLAY=:0 && su -c "cvlc --no-osd --no-video-title-show --network-caching=300 rtp://@:7236" letsving
+export DISPLAY=:0 && export XDG_RUNTIME_DIR=/run/user/$(id -u letsving) && su -c "cvlc --no-osd --no-video-title-show --network-caching=300 rtp://@:7236" letsving
 #export DISPLAY=:0 && su -c "cvlc --no-osd --no-video-title-show --network-caching=200 --no-ts-trust-pcr --h264-fps=23 rtp://@:7236" letsving
